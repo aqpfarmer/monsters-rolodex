0f51458c16cb0f5d8d115f3b307102e0483509e1
Frikkin Genius <aqpfarmer@gmail.com>
2019-07-31 09:52:11 -0400

    Updates

---
+++ precache-manifest.49a4fd1b5b0b5d8790350003b25154f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebaf60e26746135fde90a35956225594",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "bffbd9b58ac00fa4300d",
+    "url": "/monsters-rolodex/static/css/main.dc1d8565.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "bffbd9b58ac00fa4300d",
+    "url": "/monsters-rolodex/static/js/main.0d246c6d.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
